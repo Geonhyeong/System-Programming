e014628d0362b7a33a59377cc2d7bffca73c44ff
meaning2415 <meaning2415@gmail.com>
2018-12-21 16:55:00 +0900

    back

--- Tetris_Main.c
+++ Tetris_Main.c
@@ -8,36 +8,204 @@
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <time.h>
+#include<locale.h>
+#include<ncursesw/curses.h>
+#include "Tetris_GameStart2P.h"
 #include "Tetris_GameStart.h"
 #include "Tetris_Result.h"
 #include "Tetris_Search.h"
 
 int game = GAME_END; /*게임 상태 변수, 게임이 시작되거나 종료됨에 따라 변한다*/
+char i_block[4][4][4] =
+	{
+			{0, 0, 0, 0,
+			 0, 0, 0, 0,
+			 1, 1, 1, 1,
+			 0, 0, 0, 0},
 
+			{0, 0, 1, 0,
+			 0, 0, 1, 0,
+			 0, 0, 1, 0,
+			 0, 0, 1, 0},
+
+			{0, 0, 0, 0,
+			 0, 0, 0, 0,
+			 1, 1, 1, 1,   
+			 0, 0, 0, 0},
+
+			{0, 0, 1, 0,
+			 0, 0, 1, 0,
+			 0, 0, 1, 0,
+			 0, 0, 1, 0}
+	};
+
+char t_block[4][4][4] =
+
+	{
+			{0, 1, 0, 0,
+			 0, 1, 1, 0,
+			 0, 1, 0, 0,
+			 0, 0, 0, 0},
+
+			{0, 0, 0, 0,
+			 1, 1, 1, 0,
+			 0, 1, 0, 0,
+			 0, 0, 0, 0},
+
+			{0, 1, 0, 0,
+			 1, 1, 0, 0,
+			 0, 1, 0, 0,
+			 0, 0, 0, 0},
+
+			{0, 1, 0, 0,
+			 1, 1, 1, 0,
+			 0, 0, 0, 0,
+			 0, 0, 0, 0}
+	};
+
+char s_block[4][4][4] =
+	{
+			{1, 0, 0, 0,
+			 1, 1, 0, 0,
+			 0, 1, 0, 0,
+			 0, 0, 0, 0},
+
+			{0, 1, 1, 0,
+			 1, 1, 0, 0,
+			 0, 0, 0, 0,
+			 0, 0, 0, 0},
+
+			{0, 1, 0, 0,
+			 0, 1, 1, 0,
+			 0, 0, 1, 0,
+			 0, 0, 0, 0},
+
+			{0, 0, 0, 0,
+			 0, 1, 1, 0,
+			 1, 1, 0, 0,
+			 0, 0, 0, 0}
+	};
+
+char z_block[4][4][4] =
+	{
+			{0, 1, 0, 0,
+			 1, 1, 0, 0,
+			 1, 0, 0, 0,
+			 0, 0,0, 0},
+
+			{1, 1, 0, 0,
+			 0, 1, 1, 0,
+			 0, 0, 0, 0,
+			 0, 0, 0, 0},
+
+			{0, 0, 1, 0,
+			 0, 1, 1, 0,
+			 0, 1, 0, 0,
+			 0, 0, 0, 0},
+
+			{0, 0, 0, 0,
+			 1, 1, 0, 0,
+			 0, 1, 1, 0,
+			 0, 0, 0, 0}
+	};
+
+char l_block[4][4][4] =
+	{
+			{0,1, 0, 0, 
+			 0,1, 0, 0, 
+			 0,1, 1, 0, 
+			 0,0, 0, 0},
+
+			{0, 0, 0, 0,
+			 1, 1, 1, 0,
+			 1, 0, 0, 0,
+			 0, 0, 0, 0},
+
+			{1, 1, 0,0,
+			 0, 1, 0,0,
+			 0, 1, 0,0,
+			 0, 0, 0,0},
+
+			{0, 0, 1, 0,
+			 1, 1, 1, 0,
+			 0, 0, 0, 0,
+			 0, 0, 0, 0}
+	};
+
+char j_block[4][4][4] =
+	{
+			{0, 1, 0, 0,
+			 0, 1, 0, 0,
+			 1, 1, 0, 0,
+			 0, 0, 0, 0},
+
+			{1, 0, 0, 0,
+			 1, 1, 1, 0,
+			 0, 0, 0, 0,
+			 0, 0, 0, 0},
+
+			{0,1, 1, 0, 
+			 0,1, 0, 0, 
+			 0,1, 0, 0, 
+			 0,0, 0, 0},
+
+			{0, 0, 0, 0,
+			 1, 1, 1, 0,
+			 0, 0, 1, 0,
+			 0, 0, 0, 0}
+	};
+
+char o_block[4][4][4] =
+	{
+			{1, 1, 0, 0,
+			 1, 1, 0, 0,
+			 0, 0, 0, 0,
+			 0, 0, 0, 0},
+
+			{1, 1, 0, 0,
+			 1, 1, 0, 0,
+			 0, 0, 0, 0,
+			 0, 0, 0, 0},
+
+			{1, 1, 0, 0,
+			 1, 1, 0, 0,
+			 0, 0, 0, 0,
+			 0, 0, 0, 0},
+
+			{1, 1, 0, 0,
+			 1, 1, 0, 0,
+			 0, 0, 0, 0,
+			 0, 0, 0, 0}
+	};
 int display_menu(void); /* 메뉴를 보여줌 */
 
 int main(void)
 {
 	int menu = 1;
 
-	while(1)
+	while(menu)
 	{
 		menu = display_menu();
 
 		if(menu == 1)
 		{
 			game = GAME_START;
-			game_start();
+			menu = game_start();
 		}
 		else if(menu == 2)
 		{
-			search_result();
+			game = GAME_START;
+			menu = game_start_2p();
 		}
 		else if(menu == 3)
 		{
-			print_result();
+			search_result();
 		}
 		else if(menu == 4)
+		{
+			print_result();
+		}
+		else if(menu == 5)
 		{
 			system("clear");
 			exit(0);
@@ -62,14 +230,15 @@ int display_menu(void)
 		printf("\n\t\t\t■ ■ □ ■ □ □ □ ■ ■ ■ □ ■ ■ □ ");//sleep(1);refresh();
 		printf("\n\t\t\t\tT E T R I S"); 
 		printf("\n\t\t\tPlease Enter Any Key to Start..");
-		printf("\n\t\t\t  △   : Shift");
-		printf("\n\t\t\t◁   ▷ : Left / Right");
+		printf("\n\t\t\t  △   : 1p - w, 2p - i");
+		printf("\n\t\t\t◁   ▷ : 1p - a/d, 2p - j/l");
 		printf("\n\t\t\t  ▽   : Soft Drop");
 		printf("\n\t\t\tspace bar : Hard Drop");
-		printf("\n\n\t\t\t1) 게임 시작");
-		printf("\n\t\t\t2) 기록 검색");
-		printf("\n\t\t\t3) 기록 출력");
-		printf("\n\t\t\t4) 종료");
+		printf("\n\n\t\t\t1) 혼자서 하기");
+		printf("\n\t\t\t2) 둘이서 하기");
+		printf("\n\t\t\t3) 기록 검색");
+		printf("\n\t\t\t4) 기록 출력");
+		printf("\n\t\t\t5) 종료");
 		printf("\n\n\t\t\t\t\t 선택 : ");
 
 		scanf("%d",&menu);
@@ -85,4 +254,60 @@ int display_menu(void)
 	return 0;
 }
 
+/*문자를 바로 입력 받을 수 있는 함수*/
+int _getch(void)
+{
+             char   ch;
+             int   error;
+             static struct termios Otty, Ntty;
 
+             fflush(stdout);
+             tcgetattr(0, &Otty);
+             Ntty = Otty;
+             Ntty.c_iflag  =  0;
+             Ntty.c_oflag  =  0;
+             Ntty.c_lflag &= ~ICANON;
+#if 1
+            Ntty.c_lflag &= ~ECHO;
+#else
+            Ntty.c_lflag |=  ECHO;
+#endif
+            Ntty.c_cc[VMIN]  = CCHAR;
+            Ntty.c_cc[VTIME] = CTIME;
+
+#if 1
+#define FLAG TCSAFLUSH
+#else
+#define FLAG TCSANOW
+#endif
+
+            if (0 == (error = tcsetattr(0, FLAG, &Ntty)))
+            {
+                       error  = read(0, &ch, 1 );
+                       error += tcsetattr(0, FLAG, &Otty);
+            }
+
+            return (error == 1 ? (int) ch : -1 );
+}
+
+
+int set_ticker(int game_state)
+{
+        struct itimerval new_timeset;
+	
+	if(game_state==GAME_START){
+		new_timeset.it_interval.tv_sec  = 0;        /* set reload  */
+		new_timeset.it_interval.tv_usec = 1;      /* new ticker value */
+		new_timeset.it_value.tv_sec     = 0  ;      /* store this   */
+		new_timeset.it_value.tv_usec    = 1 ;     /* and this     */
+	}
+	else if(game_state==GAME_END){
+		new_timeset.it_interval.tv_sec  = 0;        /* set reload  */
+		new_timeset.it_interval.tv_usec = 0;      /* new ticker value */
+		new_timeset.it_value.tv_sec     = 0  ;      /* store this   */
+		new_timeset.it_value.tv_usec    = 0 ;     /* and this     */
+	}
+	else return 1;
+
+	return setitimer(ITIMER_VIRTUAL, &new_timeset, NULL);
+}
